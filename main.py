f009ccfafaed0722b7427d4569da919a4c702838
Kunlun Zhu <516455665@qq.com>
2019-04-09 15:58:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -43,6 +43,24 @@ parser.add_argument('--log-interval', type=int, default=25000, metavar='STEPS', 
 parser.add_argument('--render', action='store_true', help='Display screen (testing only)')
 
 
+def eval_hessian(loss_grad, model):
+    cnt = 0
+    for g in loss_grad:
+        g_vector = g.contiguous().view(-1) if cnt == 0 else torch.cat([g_vector, g.contiguous().view(-1)])
+        cnt = 1
+    l = g_vector.size(0)
+    hessian = torch.zeros(l, l)
+    for idx in range(l):
+        grad2rd = autograd.grad(g_vector[idx], model.parameters(), create_graph=True)
+        cnt = 0
+        for g in grad2rd:
+            g2 = g.contiguous().view(-1) if cnt == 0 else torch.cat([g2, g.contiguous().view(-1)])
+            cnt = 1
+        hessian[idx] = g2
+    return hessian.cpu().data.numpy()
+
+
+
 # Setup
 args = parser.parse_args()
 print(' ' * 26 + 'Options')
